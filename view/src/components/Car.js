10c8b14c60d2b0627f61203cf375fb0e9f5510ef
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-02-28 14:28:12 +0000

    Edit button click handler

--- view/src/components/Car.js
+++ view/src/components/Car.js
@@ -1,35 +1,48 @@
 // Car comp
 
+// state needed to track isEdit
+import { useState } from "react";
+
+//imp componenets
+import Form from "./Form";
+
 // Store for car type urls -
 // 4x4 - https://www.kayak.co.uk/c/wp-content/uploads/sites/198/2020/09/06_suv-medium_red-1.png
 // Compact - https://www.kayak.co.uk/c/wp-content/uploads/sites/198/2020/09/02_economy_red-1.png
 // Saloon - https://www.kayak.co.uk/c/wp-content/uploads/sites/198/2020/09/04-saloon-standard.png
 
-function handleDelete(e) {
-  console.log("Delete Request " + e.target.value);
-  // DELETE fetch call
-  // http://localhost:5000/cars/63fd052d2704c13c2c58cfbc
-  fetch("http://localhost:5000/cars/", {
-    method: "DELETE",
-    body: JSON.stringify({
-      id: e.target.value,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((res) => console.log(`Car deleted - ${res}`))
-    .then(window.location.reload(true)) // "force" page rerender to update cars display.
-    .catch((err) => console.error(`Error with request - ${err}`));
-}
-
-function handleEdit(e) {
-  console.log("Edit Request " + e.target.value);
-}
-
 // Takes props of a car object
-
+// conditional render (if Edit button is clicked (isEdit == true)) then render edit form. Else render car info
 function Car({ ...car }) {
+  // State to track isEdit and carData
+  let [isEdit, setIsEdit] = useState(false);
+
+  // func to handle delete button click
+  function handleDelete(e) {
+    console.log("Delete Request " + e.target.value);
+    // DELETE fetch call
+    fetch("http://localhost:5000/cars/", {
+      method: "DELETE",
+      body: JSON.stringify({
+        id: e.target.value,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => console.log(`Car deleted - ${res}`))
+      .then(window.location.reload(true)) // "force" page rerender to update cars display.
+      .catch((err) => console.error(`Error with request - ${err}`));
+  }
+
+  // func to handle edit button click
+  function handleEdit(e) {
+    console.log("Edit Request " + e.target.value);
+    // update isEdit state to cause render of edit form comp
+    setIsEdit((prevState) => (prevState = !prevState));
+  }
+
+  // set image using car.type value.
   let typeImg;
   switch (car.type) {
     case "4x4":
@@ -47,7 +60,9 @@ function Car({ ...car }) {
     default:
       break;
   }
-  return (
+  return isEdit ? (
+    <Form id={car._id} />
+  ) : (
     <div className="car">
       <img src={typeImg} alt={car.make + " " + car.model} />
       {/* Do map of car?? for each props render a h4. Don't include _id & __v*/}
---
+++ view/src/components/Form.js
@@ -0,0 +1,5 @@
+function Form(props) {
+  return <h1>Form for {props.id}</h1>;
+}
+
+export default Form;
